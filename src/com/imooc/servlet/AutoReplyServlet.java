f566209b45df81478a61d51ec121cfe04440fc80
learnGithubChen <729245768@qq.com>
2017-07-14 21:16:15 +0800

    自动回复的servlet

---
+++ src/com/imooc/servlet/AutoReplyServlet.java
@@ -0,0 +1,32 @@
+package com.imooc.servlet;
+
+import com.imooc.service.QueryService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by peterchen on 2017/7/9.
+ */
+public class AutoReplyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        QueryService queryService = new QueryService();
+        String result = queryService.queryByCommand(req.getParameter("content"));
+        out.write(result);
+        out.flush();
+        out.close();
+    }
+}
