e388abf3e1f0447833613dc7def819d6f04d9a64
learnGithubChen <729245768@qq.com>
2017-07-14 20:58:52 +0800

    处理微信公众号界面初始化

---
+++ src/com/imooc/servlet/InitTalkServlet.java
@@ -0,0 +1,23 @@
+package com.imooc.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by peterchen on 2017/7/9.
+ */
+public class InitTalkServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        req.getRequestDispatcher("WEB-INF/jsp/front/talk.jsp").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req,resp);
+    }
+}
