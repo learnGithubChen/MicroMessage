9715d2da6179fbc1a298ca0d98e0a2407c6ccad0
learnGithubChen <729245768@qq.com>
2017-07-14 20:45:18 +0800

    添加命令-内容类

---
+++ src/com/imooc/bean/CommandContent.java
@@ -0,0 +1,38 @@
+package com.imooc.bean;
+
+/**
+ * Created by peterchen on 2017/7/9.
+ * 与指令内容表对应的实体类
+ */
+public class CommandContent {
+    //主键
+    private String id;
+    //自动回复内容
+    private String content;
+    //关联的指令表主键
+    private String commandId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCommandId() {
+        return commandId;
+    }
+
+    public void setCommandId(String commandId) {
+        this.commandId = commandId;
+    }
+}
