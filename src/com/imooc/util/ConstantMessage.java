d5c50db27af0da917f758b3618f63902eabe3f11
learnGithubChen <729245768@qq.com>
2017-08-28 21:53:56 +0800

    添加字符串常量类

---
+++ src/com/imooc/util/ConstantMessage.java
@@ -0,0 +1,13 @@
+package com.imooc.util;
+
+/**
+ * Created by peterchen on 2017/8/28.
+ */
+public interface ConstantMessage {
+    /**
+     * 当指令没有匹配的自动回复内容时，用此内容代替。
+     */
+    public static final String NO_MATCHING_CONTENT = "客官，你没按套路出牌……我听不懂你在说什么哎！";
+    public static final String HELP_COMMAND = "帮助";
+    public static final String CHECK_COMMAND = "查看";
+}
